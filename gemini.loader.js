692f1df8a69639e448079f62fb34ab5b4d1cf77b
Matt Rose <mrose@carpages.ca>
2014-06-11 09:04:56 -0400

    Added documentation

--- gemini.loader.js
+++ gemini.loader.js
@@ -1,9 +1,28 @@
+/**
+ * @fileoverview
+
+Loads the main Gemini object and helper functions
+
+ *
+ * @namespace gemini.loader
+ * @copyright Carpages.ca 2014
+ * @author Matt Rose <matt@mattrose.ca>
+ *
+ */
 define([], function(){
 
   var GEM = window.GEM || {};
 
   var Gemini = window.Gemini || window.GEM;
 
+  /**
+   * Get query paramaters from a URL
+   *
+   * @method
+   * @name gemini.loader#qp
+   * @param {string} str The URL to extract (optional)
+   * @return {object} A JSON object of the parameters
+  **/
   // https://github.com/youbastard/getQueryParameters
   GEM.qp = function(str) {
     return (str || document.location.search)
