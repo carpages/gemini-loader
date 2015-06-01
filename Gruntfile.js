7400b4304e32b785b0483ab7b93010932f937a56
Matt Rose <rose.matt@gmail.com>
2015-06-01 17:31:17 -0400

    Add travis test support

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,11 +26,6 @@ module.exports = function (grunt) {
         }
       }
     },
-    qunit_junit: {
-      options: {
-        dest: '_build'
-      }
-    },
     jshint: {
       options: {
         reporter: require('jshint-stylish')
@@ -49,9 +44,100 @@ module.exports = function (grunt) {
           port: 9000
         }
       }
+    },
+    'saucelabs-qunit': {
+      all: {
+        options: {
+          urls: ['http://localhost:9000/test/<%= pkg.name %>.html'],
+          build: process.env.TRAVIS_JOB_ID,
+          browsers: [
+            // iOS
+            {
+              browserName: 'iphone',
+              platform: 'OS X 10.9',
+              version: '7.1'
+            },
+            {
+              browserName: 'ipad',
+              platform: 'OS X 10.9',
+              version: '7.1'
+            },
+            // Android
+            {
+              browserName: 'android',
+              platform: 'Linux',
+              version: '4.3'
+            },
+            // OS X
+            {
+              browserName: 'safari',
+              platform: 'OS X 10.9',
+              version: '7'
+            },
+            {
+              browserName: 'safari',
+              platform: 'OS X 10.8',
+              version: '6'
+            },
+            {
+              browserName: 'firefox',
+              platform: 'OS X 10.9',
+              version: '28'
+            },
+            // Windows
+            {
+              browserName: 'internet explorer',
+              platform: 'Windows 8.1',
+              version: '11'
+            },
+            {
+              browserName: 'internet explorer',
+              platform: 'Windows 8',
+              version: '10'
+            },
+            {
+              browserName: 'internet explorer',
+              platform: 'Windows 7',
+              version: '11'
+            },
+            {
+              browserName: 'internet explorer',
+              platform: 'Windows 7',
+              version: '10'
+            },
+            {
+              browserName: 'internet explorer',
+              platform: 'Windows 7',
+              version: '9'
+            },
+            {
+              browserName: 'internet explorer',
+              platform: 'Windows 7',
+              version: '8'
+            },
+            {
+              browserName: 'firefox',
+              platform: 'Windows 7',
+              version: '29'
+            },
+            {
+              browserName: 'chrome',
+              platform: 'Windows 7',
+              version: '34'
+            },
+            // Linux
+            {
+              browserName: 'firefox',
+              platform: 'Linux',
+              version: '29'
+            }
+          ]
+        }
+      }
     }
   });
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'connect', 'qunit_junit', 'qunit']);
+  grunt.registerTask('default', ['jshint', 'connect', 'qunit']);
+  grunt.registerTask('ci', ['default'/*, 'saucelabs-qunit'*/]);
 };
