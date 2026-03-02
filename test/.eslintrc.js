14155ad7a02d1a3e9a9db4f986ffa7ebd8864df4
Jeff Andrews <jeff.andrews@carpages.ca>
2026-03-02 13:42:47 -0500

    use alpha jquery-boiler

--- test/.eslintrc.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  extends: ['carpages', 'plugin:qunit/recommended', 'plugin:qunit/two'],
-  plugins: ['qunit'],
-};
--- test/qunit.config.js
+++ test/qunit.config.js
@@ -6,9 +6,9 @@ requirejs.config({
   paths: {
     gemini: '../dist/gemini',
     qunit: 'node_modules/qunit/qunit/qunit',
-    lodash: 'node_modules/lodash/lodash.min',
-    jquery: 'node_modules/jquery/dist/jquery',
-    'jquery.boiler': 'node_modules/jquery-boiler/jquery.boiler',
-    'gemini.support': 'node_modules/gemini-support/gemini.support',
+    lodash: '../node_modules/lodash/lodash.min',
+    jquery: '../node_modules/jquery/dist/jquery',
+    'jquery.boiler': '../node_modules/jquery-boiler/jquery.boiler',
+    'gemini.support': '../node_modules/gemini-support/gemini.support',
   },
 });
