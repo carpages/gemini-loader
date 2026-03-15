da84e44889dc54263fb59a7aa7c54e261b02cb20
Jeff Andrews <jeff.andrews@carpages.ca>
2026-03-15 14:47:29 -0400

    update eslint and jquery-boiler

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,13 @@
-import { fileURLToPath } from 'node:url';
+import { URL, fileURLToPath } from 'node:url';
 
 import { defineConfig } from 'eslint/config';
 import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
 import { includeIgnoreFile } from '@eslint/compat';
 import babelParser from '@babel/eslint-parser';
+import carpagesConfig from 'eslint-config-carpages/flat';
+import globals from 'globals';
 
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
-const compat = new FlatCompat({
-  baseDirectory: fileURLToPath(new URL('.', import.meta.url)),
-});
 
 export default defineConfig([
   {
@@ -27,7 +25,15 @@ export default defineConfig([
     },
   },
 
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+
   includeIgnoreFile(gitignorePath, 'Imported .gitignore patterns'),
-  ...compat.extends('carpages'),
+  carpagesConfig,
   js.configs.recommended,
 ]);
